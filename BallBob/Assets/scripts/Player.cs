397e7b91fe3e33a831a9c7a8c1d54327c80e1f5a
Jakub47 <jakub7447@gmail.com>
2018-04-26 19:06:40 +0200

    Text+Script Added. Focus on Camera!

--- BallBob/Assets/scripts/Player.cs
+++ BallBob/Assets/scripts/Player.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.UI;
 
-public class Control : MonoBehaviour 
+public class Player : MonoBehaviour 
 {
+	//private
 	Rigidbody rb;
+	private int points;
+
+	//public
 	public float speed;
+	public Text text;
+
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+		points = 0;
 	}
 	
 	// Update is called once per frame
@@ -19,5 +27,11 @@ public class Control : MonoBehaviour
 		float vInput = CrossPlatformInputManager.GetAxis ("Vertical") * speed;
 
 		rb.AddForce (hInput,0.0f,vInput);
+	}
+
+	void Increase()
+	{
+		points++;
+		text.text = points.ToString();
 	}
 }
