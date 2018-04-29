4d875c3b931401e2655a8407f185c0fe56f8a781
Jakub47 <jakub7447@gmail.com>
2018-04-29 23:07:27 +0200

    Jostick Finished, Added new Project!

---
+++ ShipDefender/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+
+	public Rigidbody2D ship;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float hInput = CrossPlatformInputManager.GetAxis ("Horizontal") * speed * Time.deltaTime;
+
+		ship.AddForce (new Vector2 (hInput, 0.0f));
+	}
+}
