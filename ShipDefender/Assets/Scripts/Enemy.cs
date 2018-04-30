9c459171965c197d1ff572b112193b6984cd9191
Jakub47 <jakub7447@gmail.com>
2018-04-30 23:53:18 +0200

    Enemy Added. Now Create and Attach script for collider!

---
+++ ShipDefender/Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public float rotationSpeed = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Rotate(new Vector3(0,0,rotationSpeed * Time.deltaTime));
+	}
+}
