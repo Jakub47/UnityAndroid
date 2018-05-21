39f67d22d9117ace16d2924d9de45d683fb3067f
Jakub47 <jakub7447@gmail.com>
2018-05-21 23:44:53 +0200

    Project Created! Focus on getting input by dividing screen into 4 sections!

---
+++ Pong/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 topLeft = Rect (0, 0, Screen.width / 2, Screen.height / 2);
+		Vector3 bottomLeft = Rect (0, 0, Screen.width / 2, Screen.height / 2);
+
+		if (Input.touchCount > 0) {
+			if(topLeft.Contains())
+		}
+	}
+}
