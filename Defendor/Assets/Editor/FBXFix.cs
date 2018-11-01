7528dbc69063aaec6f3a7055124fb0ce42031be2
Jakub47 <jakub7447@gmail.com>
2018-11-01 01:08:35 +0100

    Projekt + skrypt + Assety

---
+++ Defendor/Assets/Editor/FBXFix.cs
@@ -0,0 +1,9 @@
+﻿using UnityEditor;
+public class FBXFix : AssetPostprocessor 
+{
+	public void OnPreprocessModel()
+	{
+		ModelImporter modelImporter = (ModelImporter) assetImporter;
+		modelImporter.globalScale = 1;
+	}
+}
